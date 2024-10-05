97de62041812347e8bcc6243c4928dd47f090880
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2024-10-05 11:57:53 +0900

    lv 5 rectangules extent calculation Developing

--- src/main/java/com/example/demo/problem/lv5/rectangulesXY.java
+++ src/main/java/com/example/demo/problem/lv5/rectangulesXY.java
@@ -1,7 +1,7 @@
 package com.example.demo.problem.lv5;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class rectangulesXY {
 
@@ -25,31 +25,51 @@ public class rectangulesXY {
 
 
     public long solution(int[][] rectangles) {
-        long answer = -1;
+        long answer = 0;
 
-        // 1. 최대, 최소 xy 값
-        int minX = Integer.MAX_VALUE;
-        int minY = Integer.MAX_VALUE;
-        int maxX =0;
-        int maxY =0;
-        int[] max = new int[4];
-        Map<Integer,Long> extent = new HashMap<>();
-        long addExtent = 0l;
-
+        Map<Integer,List<Integer>> box = new HashMap<>();
         // 2. 각 직사각형의 넓이
         for(int i = 0 ; i < rectangles.length ; i++) {
-            Long value = Long.valueOf((rectangles[i][3] - rectangles[i][1]) * (rectangles[i][2] + rectangles[i][0]));
-            extent.put(i,value);
-            addExtent += value;
+
+            for(int j = rectangles[i][2] ; j > rectangles[i][0] ; j--) {
+                List<Integer> yValue = List.of(rectangles[i][1],rectangles[i][3]);
+
+                System.out.println("x축 값 : " + j + " // 해당 축의 y값" +yValue);
+                if (box.get(j) == null || box.get(j).size() == 0) {
+                    box.put(j, yValue);
+                    answer += yValue.get(1)-yValue.get(0);
+                    System.out.println("x축 값 : " + j + " // 결과가 추가" +answer);
+                } else{
+                    List<Integer> pastValue = box.get(j);
+                    if(pastValue.get(0)<yValue.get(0)){
+                        // 기존보다 왼쪽에 값이 생성
+                    } else if()
 
 
+                    answer++;
+                    System.out.println("x축 값 : " + j + " // 낱개 결과가 추가" +answer);
+                }
+            }
 
         }
 
         // 3. 겹치는 값 확인
 
 
-
         return answer;
     }
 }
+
+/*
+
+       Set<String> checkk = new HashSet<>();
+   for(int j = rectangles[i][2] ; j > rectangles[i][0] ; j--) {
+                for(int k = rectangles[i][3] ; k > rectangles[i][1] ; k--) {
+                    if(checkk.add(j+","+k)){
+                        answer ++;
+                    }
+                }
+
+            }
+
+ */
